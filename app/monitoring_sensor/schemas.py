8e26d8aa95c8fe40595e5bd24395ca693ce805de
Lourenço Velez <lourencov@HCGROUP.CA>
2025-07-07 14:22:06 -0400

    Rollback Kafka + Sensor Schema

--- app/monitoring_sensor/schemas.py
+++ app/monitoring_sensor/schemas.py
@@ -49,4 +49,3 @@ class MonitoringSensorName(BaseModel):
 
 class MonitoringSensorNameWithFields(MonitoringSensorName):
     fields: List[MonitoringSensorFieldName] = Field(default_factory=list)
-        from_attributes  = True
